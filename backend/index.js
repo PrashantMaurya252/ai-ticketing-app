1d877c9e2705124b63bc591d78f56cc7f53d4050
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-08-12 00:02:01 +0530

    setting up nodemailer

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config()
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{console.log("MongoDB connected")
+app.listen(PORT,()=>console.log("Server at http://localhost:3000"))
+}).catch((err)=>console.log("MongoDB error",err))
