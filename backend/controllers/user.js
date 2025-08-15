442dd28c89165476d71ca7757fc9a3e78e47df47
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-08-15 23:08:21 +0530

    login, logout controllers and authentication middleware

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,26 +1,78 @@
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import User from '../models/user.js'
-import { inngest } from '../ingest/client.js'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+import { inngest } from "../ingest/client.js";
 
-export const signup = async(req,res)=>{
-    const {email,password,skills=[]} = req.body
-    try {
-        const hashedPassword = bcrypt.hash(password,10)
-        const user = await User.create({email,password:hashedPassword,skills})
+export const signup = async (req, res) => {
+  const { email, password, skills = [] } = req.body;
+  try {
+    const hashedPassword = bcrypt.hash(password, 10);
+    const user = await User.create({ email, password: hashedPassword, skills });
 
-        // ingest event
-        await inngest.send({
-            name:"user/signup",
-            data:{
-                email
-            }
-        })
+    // ingest event
+    await inngest.send({
+      name: "user/signup",
+      data: {
+        email,
+      },
+    });
 
-        const token =jwt.sign({_id:user._id,role:user.role},process.env.JWT_SECRET)
+    const token = jwt.sign(
+      { _id: user._id, role: user.role },
+      process.env.JWT_SECRET
+    );
 
-        res.json({user,token})
-    } catch (error) {
-        res.status(500).json({error:"Signup failed",details:error.message})
+    res.json({ user, token });
+  } catch (error) {
+    res.status(500).json({ error: "Signup failed", details: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        error: "User not found",
+      });
     }
-}
+
+    const isMatch = bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ error: "error invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { _id: user._id, role: user.role },
+      process.env.JWT_SECRET
+    );
+    res.json({ user, token });
+  } catch (error) {
+    res.status(500).json({ error: "Login failed", details: error.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        error: "Unauthorized",
+      });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+      if (error) {
+        return res.status(401).json({
+          error: "Unauthorized",
+        });
+      }
+    });
+
+    res.json({message:"Logout Successfully!"})
+  } catch (error) {
+    res.status(500).json({ error: "Logout failed", details: error.message });
+  }
+};
